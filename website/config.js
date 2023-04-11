2f36ad87df869c1669ca3edd26f790533afeef18
jipx <pengxiang00@gmail.com>
2023-04-11 16:50:37 +0800

    Add files via upload

---
+++ website/config.js
@@ -0,0 +1,4 @@
+window.COFFEE_CONFIG = {
+	API_GW_BASE_URL_STR: "https://0rlysdneih.execute-api.us-east-1.amazonaws.com/prod",
+	COGNITO_LOGIN_BASE_URL_STR: "https://thecafe.auth.us-east-1.amazoncognito.com/login?client_id=6af9lhc9pb49dag2gfu2bpg340&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fdlnq0lt3ho2ok.cloudfront.net%2Fcallback.html"
+};
